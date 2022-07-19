d73a75755dcc6b5d651100ab0d690d3299cda228
Michael Woolnough <michael.woolnough@gmail.com>
2022-07-19 15:49:42 +0100

    added GetCommit method

--- git.go
+++ git.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"compress/zlib"
 	"errors"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
+	"strconv"
+	"time"
 )
 
 type Repo struct {
@@ -88,4 +91,91 @@ func (r *Repo) getObject(id string) (io.ReadCloser, error) {
 		Reader: z,
 		Closer: f,
 	}, nil
+}
+
+type Commit struct {
+	Tree, Parent, Msg string
+	Time              time.Time
+}
+
+func (r *Repo) GetCommit(id string) (*Commit, error) {
+	o, err := r.getObject(id)
+	if err != nil {
+		return nil, fmt.Errorf("error while opening lastCommit: %w", err)
+	}
+	buf, err := io.ReadAll(o)
+	o.Close()
+	if err != nil {
+		return nil, err
+	}
+	if string(buf[:7]) != "commit " {
+		return nil, errors.New("not a commit")
+	}
+	buf = buf[7:]
+	var l uint64
+	for n, c := range buf {
+		if c == 0 {
+			if l, err = strconv.ParseUint(string(buf[:n]), 10, 64); err != nil {
+				return nil, err
+			}
+			buf = buf[n+1:]
+			break
+		} else if c < '0' || c > '9' {
+			return nil, errors.New("invalid length")
+		}
+	}
+	if l == 0 {
+		return nil, errors.New("zero commit size")
+	} else if l != uint64(len(buf)) {
+		return nil, errors.New("invalid commit size")
+	}
+	c := new(Commit)
+	for {
+		p := bytes.IndexByte(buf, '\n')
+		line := buf[:p]
+		buf = buf[p+1:]
+		if p == 0 {
+			break
+		} else if p < 0 {
+			return nil, errors.New("invalid commit")
+		}
+		if p > 5 && string(line[:5]) == "tree " {
+			if c.Tree == "" {
+				if c.Tree = checkSHA(line[5:]); c.Tree == "" {
+					return nil, errors.New("invalid tree SHA")
+				}
+			}
+		} else if p > 7 && string(line[:7]) == "parent " {
+			if c.Parent == "" {
+				if c.Parent = checkSHA(line[7:]); c.Parent == "" {
+					return nil, errors.New("invalid parent SHA")
+				}
+			}
+		} else if p > 10 && string(line[:10]) == "committer " {
+			if c.Time.IsZero() {
+				line = line[10:]
+				z := bytes.LastIndexByte(line, ' ')
+				if z < 0 {
+					return nil, errors.New("invalid timezone")
+				}
+				zoneOffset, err := strconv.ParseInt(string(line[z+1:]), 10, 16)
+				if err != nil {
+					return nil, errors.New("invalid timezone string")
+				}
+				hours := zoneOffset / 100
+				mins := zoneOffset % 100
+				s := bytes.LastIndexByte(line[:z], ' ')
+				if s < 0 {
+					return nil, errors.New("invalid timestamp")
+				}
+				unix, err := strconv.ParseInt(string(line[s+1:z]), 10, 64)
+				if err != nil {
+					return nil, fmt.Errorf("invalid timestamp string: %w", err)
+				}
+				c.Time = time.Unix(unix, 0).In(time.FixedZone("UTC", int(hours*3600+mins*60)))
+			}
+		}
+	}
+	c.Msg = string(buf[:len(buf)-1])
+	return c, nil
 }
