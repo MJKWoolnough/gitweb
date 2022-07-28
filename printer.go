82011f1855f3187ae90cb613018d1bbeddd3a8ae
Michael Woolnough <michael.woolnough@gmail.com>
2022-07-28 19:16:22 +0100

    optimise printer for accepting a memio.LimitedBuffer

--- printer.go
+++ printer.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io"
 
+	"vimagination.zapto.org/memio"
 	"vimagination.zapto.org/parser"
 	"vimagination.zapto.org/rwcount"
 )
@@ -44,8 +45,17 @@ func prettify(file *File, w io.Writer, r io.Reader, tf parser.TokenFunc) (int64,
 	c := make(chan parser.Phrase)
 	e := make(chan error)
 	go handleTemplate(file, w, c, e)
-	rw := rwcount.Reader{Reader: r}
-	tk := parser.New(parser.NewReaderTokeniser(&rw))
+	var (
+		rw rwcount.Reader
+		tk parser.Parser
+	)
+	if lr, ok := r.(*memio.LimitedBuffer); ok {
+		rw.Count = int64(len(*lr))
+		tk = parser.New(parser.NewByteTokeniser(*lr))
+	} else {
+		rw.Reader = r
+		tk = parser.New(parser.NewReaderTokeniser(&rw))
+	}
 	tk.TokeniserState(tf)
 	tk.PhraserState(lines)
 	for {
